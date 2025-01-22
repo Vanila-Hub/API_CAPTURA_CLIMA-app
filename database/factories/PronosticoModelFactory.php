d13863db3a6ae13010e694f31430c56d9b979fc2
Vanila-Dev <149675635+Vanila-Hub@users.noreply.github.com>
2025-01-22 22:50:44 +0100

    hope i make out hereee

--- database/factories/PronosticoModelFactory.php
+++ database/factories/PronosticoModelFactory.php
@@ -28,9 +28,9 @@ class PronosticoModelFactory extends Factory
 
         self::$timeIncrement += 15; // Incremento de 15 minutos
 
-        // Ajuste de temperatura según mes
+        // Ajuste de temperatura según mes con variación realista entre -0.5°C y 0.5°C
         $month = $date->month;
-        $temperatura = $this->getTemperatureForMonth($month) + $this->faker->randomFloat(1, -2, 2);
+        $temperatura = $this->getTemperatureForMonth($month) + $this->faker->randomFloat(1, -0.5, 0.5);  // Variación entre -0.5°C y 0.5°C
 
         // Descripciones posibles basadas en rangos de temperatura
         $descripciones = $this->getDescriptionsBasedOnTemperature($temperatura);
@@ -66,7 +66,7 @@ class PronosticoModelFactory extends Factory
             3 => 9,  // Marzo
             4 => 12, // Abril
             5 => 15, // Mayo
-            6 => 18, // Junio
+            6 => 19, // Junio
         ];
 
         return $temperatureRanges[$month] ?? 15;  // Valor por defecto si no se encuentra el mes
