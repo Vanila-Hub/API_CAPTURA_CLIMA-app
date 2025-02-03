0f623a5f7271b067ee99a86dac05b5782af3a41b
Juan Antero Asumu <149675635+Vanila-Hub@users.noreply.github.com>
2025-02-03 12:57:29 +0100

    minor changes

--- app/Models/CiudadModel.php
+++ app/Models/CiudadModel.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class CiudadModel extends Model
 {
-    protected $table = 'ciudades';   // El nombre de la tabla no será "articles" sino "articulos"
+    protected $table = 'ciudades'; 
     protected $primaryKey = 'id'; // La clave primaria no será "id" sino "id_art"
     protected $fillable = array('nombre', 'latitud','longitud'); // Campos de la tabla en los que se permite la ASIGNACIÓN MASIVA
 
