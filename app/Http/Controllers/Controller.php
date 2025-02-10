d1192a0281845f15113f5ca12369c455601f5a8d
Juan Antero Asumu <149675635+Vanila-Hub@users.noreply.github.com>
2025-02-10 12:10:57 +0100

    Minimal changes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use OpenApi\Attributes as OA;
 
 #[
-    OA\Info(version: "1.0.0", description: "Vanila Meteo Api", title: "Vanila Meteo Api"),
+    OA\Info(version: "1.0.0", description: "Vanila weather API", title: "Vanila weather API"),
     OA\Server(url: 'http://127.0.0.1:99/api', description: "local server"),
     OA\Server(url: 'http://staging.example.com', description: "staging server"),
     OA\Server(url: 'http://example.com', description: "production server"),
